c9caeccbca3b2d30e8c499a42b0299175595cc43
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-04-12 23:59:43 +0000

    Adding file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Actors, MovieInformation, Movies, NavBar, Profile } from ".";
+import useStyles from "./styles";
 
-const App = () => <div>App</div>;
+const App = () => {
+  const classes = useStyles();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 export default App;
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MovieInformation = () => {
+  console.log("Movie information");
+  return <div>MovieInformation</div>;
+};
+
+export default MovieInformation;
