b64396dc1e633d0c0fdb4195932ba33fc6151b8b
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-04-18 18:07:15 +0100

    Rendering movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -7,11 +7,37 @@ import {
 } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useGetMoviesQuery } from "../../services/TMDB";
+import MovieList from "../MovieList/MovieList";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
-  return <div>Movies</div>;
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "An error has occured.";
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
