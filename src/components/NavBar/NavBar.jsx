a5e21d097ee68b63bdf58acd364f3eb673ce2bab
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-05-22 20:55:11 +0100

    Adding Auth

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   AppBar,
@@ -16,7 +16,10 @@ import {
   Brightness7,
 } from "@mui/icons-material";
 import { useTheme } from "@mui/material/styles";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, userSelector } from "../../features/auth";
 import { Sidebar, Search } from "..";
+import { fetchToken, createSessionId, moviesApi } from "../../utils";
 import useStyles from "./styles";
 
 const NavBar = () => {
@@ -24,7 +27,35 @@ const NavBar = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(max-width:600px)");
   const theme = useTheme();
-  const isAuthenticated = true;
+  const { isAuthenticated, user } = useSelector(userSelector);
+
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("request_token");
+  const sessionIdFromLocalStorage = localStorage.getItem("session_id");
+
+  useEffect(() => {
+    const logInUser = async () => {
+      if (token) {
+        if (sessionIdFromLocalStorage) {
+          const { data: userData } = await moviesApi.get(
+            `/account?session_id=${sessionIdFromLocalStorage}`
+          );
+
+          dispatch(setUser(userData));
+        } else {
+          const sessionId = await createSessionId();
+
+          const { data: userData } = await moviesApi.get(
+            `/account?session_id=${sessionId}`
+          );
+
+          dispatch(setUser(userData));
+        }
+      }
+    };
+
+    logInUser();
+  }, [token]);
 
   return (
     <>
@@ -47,14 +78,14 @@ const NavBar = () => {
           {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
-              <Button color="inherit" onClick={() => {}}>
+              <Button color="inherit" onClick={fetchToken}>
                 Login &nbsp; <AccountCircle />
               </Button>
             ) : (
               <Button
                 color="inherit"
                 component={Link}
-                to="/profile/:id"
+                to={`/profile/${user.id}`}
                 className={classes.linkButton}
                 onClick={() => {}}
               >
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Typography, Button, Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import { ExitToApp } from "@mui/icons-material";
+import { userSelector } from "../../features/auth";
 
 const Profile = () => {
-  console.log("Profile");
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+  console.log(user);
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = "/";
+  };
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favorite Movies</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
